259c3025ce24e2b67cfb7bac9dc248b1d23c428a
tmeserve <31040347+tmeserve@users.noreply.github.com>
2017-08-21 09:38:46 -0500

    Update Report.java

--- Report.java
+++ Report.java
@@ -6,17 +6,22 @@ public class Report {
 
 	public static void main(String[] args) {
 		
+		// User input scanner
 		Scanner Fah = new Scanner(System.in);
 		
+		// Choose your degrees
 		System.out.println("Welcome. Please enter your temperature in degrees F.");
 		int F = Fah.nextInt();
 		
+		// Math
 		double C = 5.0 / 9.0 * (F-32.0);
 		
+		// If it's cold or not
 		if (F <= 32)
 		{
 			System.out.println("Your temperature is cold. Burrrr.");
 		}
+		// If it's hot or not
 		else if (F >= 90)
 		{
 			System.out.println("Your temperature is hot. Don't get a sunburn.");
@@ -25,6 +30,7 @@ public class Report {
 			
 		}
 		
+		// Degrees in centigrade
 		System.out.println("Your temperature is also " + C + " degrees centigrade.");
 		System.out.println("Have a nice day!");
 		
